3e7b818f3462fa6544f68f348e6c1f2817ff54c5
Golodhrim <holidayninjastaff@gmail.com>
2026-08-31 00:10:35 -0400

    database update

--- aggregator_project/settings.py
+++ aggregator_project/settings.py
@@ -82,10 +82,11 @@ WSGI_APPLICATION = 'aggregator_project.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
